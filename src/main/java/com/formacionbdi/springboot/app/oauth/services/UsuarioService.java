598e30c2d6b82fce208272cad6d714fb6ed62507
Saul Avila <saul_harry_@hotmail.com>
2019-07-31 14:16:41 -0500

    se comienza configurar la utenticacion usando spring security y feign client

---
+++ src/main/java/com/formacionbdi/springboot/app/oauth/services/UsuarioService.java
@@ -0,0 +1,49 @@
+package com.formacionbdi.springboot.app.oauth.services;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.formacionbdi.springboot.app.oauth.clients.UsuarioFeignClient;
+import com.formacionbdi.springboot.app.usuarios.commons.models.entity.Usuario;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Service
+public class UsuarioService implements UserDetailsService {
+
+	@Autowired
+	private UsuarioFeignClient client;
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		Usuario usuario = client.findByUsername(username);
+
+		if (usuario == null) {
+			log.error("Error en el login, no existe el usuario '{}' en el sistema", username);
+			throw new UsernameNotFoundException(
+					"Error en el login, no existe el usuario '" + username + "' en el sistema");
+		}
+
+		List<GrantedAuthority> authorities = 
+				usuario.getRoles()
+				.stream()
+				.map(role -> {
+					return new SimpleGrantedAuthority(role.getNombre());
+				})
+				.peek(authority -> log.info("Role: {}", authority.getAuthority()))
+				.collect(Collectors.toList());
+		log.info("Usuario Autenticado {}", username);
+		return new User(username, usuario.getPassword(), usuario.getEnabled(), true, true, true, authorities);
+	}
+
+}
